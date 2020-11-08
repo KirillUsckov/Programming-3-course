2b85e7efa556a9f19268608f8c45320180015efa
KirillUsckov <kir.usckov@gmail.com>
2020-11-08 13:11:45 +0300

    Done car project

---
+++ Car/Car/Auto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Auto
+    {
+        private static int tankVolume, gasolineConsumption; //объем бака + расход бензина
+        
+        public static int getDistanceForGasolin(int tankVolume, int gasolineConsumption)
+        {
+            return tankVolume / gasolineConsumption;
+        }
+    }
+}
---
+++ Car/Car/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите объем бака (л): ");
+            String input = Console.ReadLine();
+            int tank = int.Parse(input);
+            Console.Write("Введите расход бензина (л на 1 км): ");
+            input = Console.ReadLine();
+            int gasoline = int.Parse(input);
+            Console.Write("Введите длину пути (км): ");
+            input = Console.ReadLine();
+            int distance = int.Parse(input);
+            Car car = new Car(gasoline, tank, distance);
+
+            Car.refuelingAmount();
+            Car.gasolineForRefueling();
+            Car.distanceForRefueling();
+            Car.theLastRefueling();
+            Console.ReadKey();
+        }
+    }
+}
